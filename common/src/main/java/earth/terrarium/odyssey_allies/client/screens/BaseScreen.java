358030afadf09a91bc30dfa4b60033275159d7bc
Mapter <mapter0k@gmail.com>
2026-07-08 20:41:50 +0500

    gui navigation

--- common/src/main/java/earth/terrarium/odyssey_allies/client/screens/BaseScreen.java
+++ common/src/main/java/earth/terrarium/odyssey_allies/client/screens/BaseScreen.java
@@ -3,10 +3,15 @@ package earth.terrarium.odyssey_allies.client.screens;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.teamresourceful.resourcefullib.client.CloseablePoseStack;
 import com.teamresourceful.resourcefullib.client.screens.BaseCursorScreen;
+import com.teamresourceful.resourcefullib.client.screens.ScreenHistory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
 
-public abstract class BaseScreen extends BaseCursorScreen {
+public abstract class BaseScreen extends BaseCursorScreen implements ScreenHistory {
 
     protected final int imageWidth;
     protected final int imageHeight;
@@ -15,12 +20,16 @@ public abstract class BaseScreen extends BaseCursorScreen {
     protected int titleLabelX;
     protected int titleLabelY;
 
+    @Nullable
+    private Screen lastScreen;
+
     public BaseScreen(Component displayName, int imageWidth, int imageHeight) {
         super(displayName);
         this.imageWidth = imageWidth;
         this.imageHeight = imageHeight;
         this.titleLabelX = 8;
         this.titleLabelY = 6;
+        this.lastScreen = Minecraft.getInstance().screen;
     }
 
     @Override
@@ -39,6 +48,31 @@ public abstract class BaseScreen extends BaseCursorScreen {
             this.renderLabels(graphics, mouseX, mouseY);
         }
         RenderSystem.enableDepthTest();
+    }
+
+    @Override
+    public void setLastScreen(@Nullable Screen screen) {
+        this.lastScreen = screen;
+    }
+
+    @Override
+    @Nullable
+    public Screen getLastScreen() {
+        return this.lastScreen;
+    }
+
+    @Override
+    public boolean canGoBack() {
+        return this.lastScreen != null;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == GLFW.GLFW_KEY_ESCAPE && this.canGoBack()) {
+            this.goBack();
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
     protected abstract void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY);
