5e08a01f7b336e439e57d677b64cafbb9ba6e0af
mapter <mapter0k@gmail.com>
2026-09-13 09:28:10 +0500

    add parent to member settings

--- common/src/main/java/earth/terrarium/argonauts/api/teams/settings/MemberSetting.java
+++ common/src/main/java/earth/terrarium/argonauts/api/teams/settings/MemberSetting.java
@@ -2,5 +2,13 @@ package earth.terrarium.argonauts.api.teams.settings;
 
 import net.minecraft.network.chat.Component;
 
-public record MemberSetting(String id, Component name, Component description) {
+public record MemberSetting(String id, Component name, Component description, String parent) {
+
+    public MemberSetting(String id, Component name, Component description) {
+        this(id, name, description, "");
+    }
+
+    public boolean hasParent() {
+        return !this.parent.isEmpty();
+    }
 }
